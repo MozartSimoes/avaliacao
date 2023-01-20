c481f3b009cab1d070583a6211d1cbefd62ab9d2
MozartSimoes <mozartmsimoes@hotmail.com>
2023-01-20 09:31:13 -0300

    Update SQLCalcIdade.sql

--- SQLCalcIdade.sql
+++ SQLCalcIdade.sql
@@ -2,4 +2,5 @@ DECLARE @nasc as varchar(11)='17/04/2008';
 
 DECLARE @Hoje DATETIME = GETDATE();
 
-SELECT FLOOR(DATEDIFF(DAY, @nasc, @Hoje) / 365.25) as IDADE -- retorna 21, idade correta
+--calculo correto fazendo pelo dia de nascimento - 14
+SELECT FLOOR(DATEDIFF(DAY, @nasc, @Hoje) / 365.25) as IDADE 
